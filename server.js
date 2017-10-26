e7e4c6af1eb90945ab88efabd653488a0364ea83
Surabhi Ravishankar <surabhi.ravishankar@surabhiravi-ltm.internal.salesforce.com>
2017-10-25 23:19:10 -0500

    localhost working

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 var AlexaAppServer = require( 'alexa-app-server' );
 
 var server = new AlexaAppServer( {
-	httpsEnabled: false,
-	port: process.env.PORT || 80
+	httpsEnabled: true,
+	port: process.env.PORT || 6090
 } );
 
 server.start();
